2bbdc22b348ff8158c6dce00a6ecfee1dc89f287
chaoweili <lcwei0521@163.com>
2014-08-06 20:05:17 +0800

    chaoweili
    0806 FBCircle

---
+++ FBCircle/Found/FoundControllers/FoundViewController.h
@@ -0,0 +1,16 @@
+//
+//  FoundViewController.h
+//  FBCircle
+//
+//  Created by soulnear on 14-8-4.
+//  Copyright (c) 2014年 soulnear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoundViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+{
+    UITableView *_tableView;//主tableview
+    
+}
+@end
