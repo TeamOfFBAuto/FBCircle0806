3ad4d0760268a883b00136f6378aca7fddaf7c1b
chaoweili <lcwei0521@163.com>
2014-08-14 15:01:41 +0800

    chaowei
    sync

--- FBCircle/PersonalAPI/FBQuanAPI.h
+++ FBCircle/PersonalAPI/FBQuanAPI.h
@@ -181,6 +181,23 @@ alpha:(a)]
 //搜索
 
 #define FBCIRCLE_SEARCH_BBS @"http://quan.fblife.com/index.php?c=forum&a=searchforum&keyword=%@&page=%d&ps=%d" //搜论坛
-
 #define FBCIRCLE_SEARCH_TOPIC @"" //搜帖子
+
+//微论坛主页 -- 帖子
+
+#define FBCIRCLE_TOPIC_LIST @"http://quan.fblife.com/index.php?c=forum&a=getthreads&fid=%@" //帖子列表
+#define FBCIRCLE_TOPIC_LIST_HOT @"http://quan.fblife.com/index.php?c=forum&a=topcomthreads" //热门帖子列表
+#define FBCIRCLE_TOPIC_LIST_MYJOIN @"http://quan.fblife.com/index.php?c=forum&a=topcomthreads&authkey=%@" //我关注的热门帖子列表
+
+#define FBCIRCLE_TOPIC_INFO @"http://quan.fblife.com/index.php?c=forum&a=viewthread&tid=%@&page=%d&ps=%d"//帖子详情
+#define FBCIRCLE_TOPIC_ZAN @"http://quan.fblife.com/index.php?c=forum&a=zanthread&authkey=%@&tid=%@"//赞帖子
+#define FBCIRCLE_TOPIC_TOP @"http://quan.fblife.com/index.php?c=forum&a=topthread&authkey=%@&fid=%@&tid=%@"//置顶帖子
+#define FBCIRCLE_TOPIC_TRIPTOP @"http://quan.fblife.com/index.php?c=forum&a=triptopthread&authkey=%@&fid=%@&tid=%@"//取消帖子置顶
+
+#define FBCIRCLE_TOPIC_DELETE @"http://quan.fblife.com/index.php?c=forum&a=delthread&authkey=%@&fid=%@&tid=%@"//删除帖子
+
+//评论
+
+#define FBCIRCLE_COMMENT_ADD @"http://quan.fblife.com/index.php?c=forum&a=addcomment&authkey=%@&content=%@&fid=1&tid=1" //发表评论
+#define FBCIRCLE_COMMENT_LIST @"http://quan.fblife.com/index.php?c=forum&a=getcomment&tid=%@&page=%d&ps=%d" //评论列表
 
